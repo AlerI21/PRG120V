800a0ecb05a0cb87222cc3e2a24a3fe5623450b4
AlerI21 <aleksander.k.e@outlook.com>
2025-09-15 17:47:59 +0200

    .

--- Tema03/Oppgave3.php
+++ Tema03/Oppgave3.php
@@ -5,14 +5,14 @@ for ($tall=1;$tall<=10;$tall++)
     print("$tall");
 }
 
-<br/>
+print("<br/>")
 
 for ($tall=11;$tall<=20;$tall++)
 {
     print("$tall");
 }
 
-<br/>
+print("<br/>");
 
 for ($tall=21;$tall<=30;$tall++)
 {
